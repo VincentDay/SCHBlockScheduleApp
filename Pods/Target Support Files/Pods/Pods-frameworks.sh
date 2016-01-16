632322aebe81f6693922a57f3d2f975061f59520
Mike Litman <mike@awesomefat.com>
2016-01-16 14:48:45 -0600

    styled buttons and set layout constraints

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,8 +86,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Bolts.framework"
   install_framework "Pods/Parse.framework"
+  install_framework "Pods/PureLayout.framework"
+  install_framework "Pods/SwiftyButton.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Bolts.framework"
   install_framework "Pods/Parse.framework"
+  install_framework "Pods/PureLayout.framework"
+  install_framework "Pods/SwiftyButton.framework"
 fi
